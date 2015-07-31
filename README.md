8873e2a78dbe73e0864aedd0322b595b714dafb1
James Bowman <james.edward.bowman@gmail.com>
2015-07-31 18:44:46 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # slacker
 Slack Out Of Office responder
+
+[Generate a Slack API Token](https://api.slack.com/web#basics) for your own user account and then Slacker will connect to Slack and respond to any messages directed to you with an out of office response.
---
+++ slacker.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/james-bowman/slack"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func main() {
+	slackToken := getToken()
+
+	conn, err := slack.Connect(slackToken)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	slack.EventProcessor(conn, handler, nil)
+}
+
+func handler(message *slack.Message) {
+	message.Respond("I am afraid I am currently out of the office on annual leave returning Monday 17th August.  If you require urgent assistance, please contact @darius")
+}
+
+// getToken for authenticating with Slack.  Ordered lookup process trying first the command line,
+// then environment variables and finally a config file
+func getToken() string {
+	var slackToken string
+
+	log.Println("Looking for Slack auth token as command line argument")
+	flag.StringVar(&slackToken, "slacktoken", "", "Slack authentication token - if not specified, will look for an environment variable or config file")
+	flag.Parse()
+
+	if slackToken == "" {
+		log.Println("Slack auth token not found - looking for environment variable")
+		slackToken = os.Getenv("SLACKER_SLACK_TOKEN")
+	}
+
+	if slackToken == "" {
+		log.Println("Slack auth token not found - looking for config file")
+		slackTokenFileName := "slack.token"
+
+		slackTokenFile, err := ioutil.ReadFile(slackTokenFileName)
+		if err != nil {
+			log.Panic(fmt.Sprintf("Error opening slack authentication token file %s: %s", slackTokenFileName, err))
+		}
+
+		slackToken = string(slackTokenFile)
+	}
+
+	if slackToken != "" {
+		log.Println("Slack auth token found")
+	}
+
+	return slackToken
+}
